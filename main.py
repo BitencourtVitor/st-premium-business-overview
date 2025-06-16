775391b4088334072927e1803107be6ac9e218c3
Vitor Bitencourt <vitormbaraujo1306@gmail.com>
2025-06-16 12:25:39 -0300

    correção main 1

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import json
 import logging
 import traceback
 from pathlib import Path
+import streamlit as st
+from database.mongodb_utils import get_collection_data
 
+try:
+    users = get_collection_data("users")
+    st.write("Usuários encontrados:", users)
+except Exception as e:
+    st.write("Erro ao acessar MongoDB:", e)
+    
 # Configuração da página
 FAVICON = "assets/premium_favicon.png"
 LOGO = "assets/premium_logo.png"
